7e54dd3a74c1b6af0c1b8e5486b2363c9e4c400b
Eli Berkowitz <eliberkowitz@gmail.com>
2024-04-20 10:57:58 -0400

    fix play icon

--- src/sections/Art.tsx
+++ src/sections/Art.tsx
@@ -184,8 +184,8 @@ function Gallery({
 }
 
 const PAUSE_ICON = <span>&#x23F8;&#xFE0E;</span>;
-const PLAY_ICON = <span>&#x23F8;&#xFE0E;</span>;
-const FULLSCREEN_ICON = <span>&#x26f6;&#xFE0E;</span>;
+const PLAY_ICON = <span>&#x25B6;&#xFE0E;</span>;
+const FULLSCREEN_ICON = <span>&#x26F6;&#xFE0E;</span>;
 
 function GalleryControls({
   numPieces,
