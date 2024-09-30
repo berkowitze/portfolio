c6f6df9eef18da52771a31d84bc86a8d776573d0
Eli Berkowitz <eliberkowitz@gmail.com>
2024-09-30 14:41:47 -0400

    add raytracer project, reorganize projects, update about

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -3,25 +3,23 @@ export default function About(): JSX.Element {
     <div className="flex flex-wrap gap-6">
       <div className="flex grow basis-2/3 flex-col gap-4">
         <p>
-          I am a full stack web developer with a passion for making beautiful
-          and engaging user experiences. I am starting to explore the field of
-          game development and am excited to be starting a Masters of Fine Arts
-          at Clark University this fall!
+          I am a video game and full stack web developer with a passion for
+          making beautiful and engaging user experiences. In September 2024, I
+          began pursuing my Master of Fine Arts in Interactive Media at Clark
+          University.
         </p>
         <p>
-          I previously worked at <a href="https://benchling.com">Benchling</a>{" "}
-          on the team implementing a new data analytics product for Biotech
-          R&amp;D acceleration.
-        </p>
-        <p>
-          I received a Bachelor of Science in Applied Mathematics from Brown
-          University in May 2019.
+          After receiving a Bachelor of Science in Applied Mathematics from
+          Brown University in 2019, I joined{" "}
+          <a href="https://benchling.com">Benchling</a>, where I contributed to
+          the development of a new data analytics product aimed at accelerating
+          Biotech R&D.
         </p>
         <p>Other fun facts:</p>
         <ol className="ml-8 list-decimal">
           <li>I own a bearded dragon!</li>
-          <li>I speak Portuguese and beginner level German</li>
-          <li>I enjoy hiking, hiking, and backpacking.</li>
+          <li>I speak Portuguese and beginner level German.</li>
+          <li>I enjoy hiking, biking, and backpacking.</li>
           <li>I play classical piano and flute.</li>
         </ol>
         <p>Please hire me.</p>
--- src/sections/Code.tsx
+++ src/sections/Code.tsx
@@ -8,11 +8,69 @@ interface CodePiece {
   video: string;
   description: string | JSX.Element;
   stack?: string;
-  company?: "Benchling";
+  company?: "Benchling" | "Clark University MFA";
 }
 
 const CODE_PIECES: ReadonlyArray<CodePiece | "professional" | "hobby"> = [
-  "hobby",
+  // "hobby",
+  {
+    id: "raytracer",
+    url: "/blog",
+    title: "C++ Raytracer",
+    video: "/Code/raytracer.mp4",
+    description: (
+      <div>
+        For my Graduate Seminar class at Clark University, I'm writing a
+        raytracer from scratch in C++. At first, it is following the well-known{" "}
+        <a href="https://raytracing.github.io/">Ray Tracing in One Weekend</a>{" "}
+        tutorials.
+        <br />
+        Next, I am augmenting the raytracer with the ability to import and
+        render FBX files, including materials, from a Blender export.
+        <br />I also implemented multiprocessing and feature to preview
+        in-progress renders.
+        <br />
+        <a href="https://github.com/berkowitze/raytracer">Code</a>
+      </div>
+    ),
+    stack: "C++",
+    company: "Clark University MFA",
+  },
+  {
+    id: "analysis-tool",
+    url: "https://help.benchling.com/hc/en-us/articles/15322155422221-Creating-analysis-tables-from-a-Registry-schema",
+    title: "Analysis Tool",
+    video: "/Code/analysis-tool.mp4",
+    description: (
+      <div>
+        I worked on a new analysis platform for Benchling customers to transform
+        and analyze their data without having to write SQL. My work included:
+        <ul className="list-disc pl-6">
+          <li>
+            A tool to create datasets with only checkboxes, building up a
+            GraphQL query under the hood which was then transformed to a dataset
+          </li>
+          <li>
+            Tools for users to add transformations to their data, such as
+            aggregations and filters
+          </li>
+          <li>Scaffolding for the product</li>
+        </ul>
+      </div>
+    ),
+    stack: "React, TypeScript, Flask, SQLAlchemy, Postgres, Redis, S3",
+    company: "Benchling",
+  },
+  {
+    id: "entity-diagram",
+    url: "https://help.benchling.com/hc/en-us/articles/9684226168077-Search-for-schemas-to-use-in-Insights-queries#h_e8f23b880b",
+    title: "Database Diagram",
+    video: "/Code/entity-diagram.mp4",
+    description:
+      "Benchling customers have their own denormalized database schemas that they can query with SQL using the Insights product. This tool helps them understand how their data is structured and interrelated.",
+    stack: "React, TypeScript",
+    company: "Benchling",
+  },
   {
     id: "downup",
     url: "https://downup.app",
@@ -56,42 +114,7 @@ const CODE_PIECES: ReadonlyArray<CodePiece | "professional" | "hobby"> = [
       "My first ever website! Shows a ticking estimate of the percentage of your life that has passed.",
     stack: "HTML, CSS, JavaScript",
   },
-  "professional",
-  {
-    id: "analysis-tool",
-    url: "https://help.benchling.com/hc/en-us/articles/15322155422221-Creating-analysis-tables-from-a-Registry-schema",
-    title: "Analysis Tool",
-    video: "/Code/analysis-tool.mp4",
-    description: (
-      <div>
-        I worked on a new analysis platform for Benchling customers to transform
-        and analyze their data without having to write SQL. My work included:
-        <ul className="list-disc pl-6">
-          <li>
-            A tool to create datasets with only checkboxes, building up a
-            GraphQL query under the hood which was then transformed to a dataset
-          </li>
-          <li>
-            Tools for users to add transformations to their data, such as
-            aggregations and filters
-          </li>
-          <li>Scaffolding for the product</li>
-        </ul>
-      </div>
-    ),
-    stack: "React, TypeScript, Flask, SQLAlchemy, Postgres, Redis, S3",
-    company: "Benchling",
-  },
-  {
-    id: "entity-diagram",
-    url: "https://help.benchling.com/hc/en-us/articles/9684226168077-Search-for-schemas-to-use-in-Insights-queries#h_e8f23b880b",
-    title: "Database Diagram",
-    video: "/Code/entity-diagram.mp4",
-    description:
-      "Benchling customers have their own denormalized database schemas that they can query with SQL using the Insights product. This tool helps them understand how their data is structured and interrelated.",
-    stack: "React, TypeScript",
-    company: "Benchling",
-  },
+  // "professional",
 ];
 
 export default function Code() {
@@ -129,6 +152,7 @@ function CodeProject({
 }) {
   return (
     <a
+      target={codePiece.url.startsWith("/") ? "_self" : "_blank"}
       key={codePiece.id}
       href={codePiece.url}
       className="no-underline-ani flex grow basis-32 cursor-pointer flex-wrap items-center justify-between gap-8 rounded-[2px] border-my-blue/30 p-4 text-left transition-colors duration-300 hover:border-my-blue hover:bg-gray-100 md:border-l-2"
@@ -137,11 +161,6 @@ function CodeProject({
         <h3 className="flex items-center gap-2 text-2xl font-bold text-black">
           {codePiece.company && `${codePiece.company} - `}
           {codePiece.title}
-          {/* {codePiece.company && (
-            <span className="rounded-full bg-my-blue px-2 py-1 text-sm text-white">
-              
-            </span>
-          )} */}
           <span className="hide-with-pointer translate-y-0.5 text-lg text-my-blue">
             {" "}
             &#8599;
