f86d39d8195a0521fdbd5461795e4f3536dd3cc9
Eli Berkowitz <eliberkowitz@gmail.com>
2025-11-25 15:28:02 -0500

    artstation logo

--- src/Util/SocialIcons.tsx
+++ src/Util/SocialIcons.tsx
@@ -55,18 +55,14 @@ export default function SocialIcons({
             xmlns="http://www.w3.org/2000/svg"
             width="24"
             height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
+            viewBox="0 0 208.8 195.9"
+            fill="currentColor"
             className="grayscale"
+            style={{ transform: "scale(1.5)" }}
           >
-            <path d="M12 2L2 7v10l10 5 10-5V7L12 2z" />
-            <path d="M12 22V12" />
-            <path d="M2 7l10 5 10-5" />
-            <path d="M7 12l5 2.5 5-2.5" />
+            <path d="M51.4,123.3l8.9,15.4l0,0c1.8,3.5,5.4,5.9,9.5,5.9l0,0l0,0h59.3l-12.3-21.3H51.4z" />
+            <path d="M157.2,123.4c0-2.1-0.6-4.1-1.7-5.8l-34.8-60.4c-1.8-3.4-5.3-5.7-9.4-5.7H92.9l53.7,93l8.5-14.7C156.7,127,157.2,125.8,157.2,123.4z" />
+            <polygon points="108.1,108.1 84.2,66.6 60.2,108.1" />
           </svg>
         }
       />
